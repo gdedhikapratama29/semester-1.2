46f4aba4201cd511566895dc640dd16d8f4c5842
gdedhikapratama29 <gdedhikapratama29@gmail.com>
2026-07-14 15:53:35 +0800

    penambahan

---
+++ data.js
@@ -0,0 +1,12 @@
+const namaSaya = "dhika";
+let usiaSaya = 20;
+
+
+function generateBiodata() {
+    console.log(`nama saya ${namaSaya} dan usia saya ${usiaSaya}`);
+}
+
+// console.log(namaSaya)
+// console.log(usiaSaya)
+
+generateBiodata();
